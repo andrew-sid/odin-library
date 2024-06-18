c7f325b405db7c9487dec8aed9cf7c1eb78a058b
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-18 17:54:09 +0700

    Add theme changing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('123');
+const myLibrary = [];
+
+function Book(title, author, numOfPages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+  return;
+}
+
+// changing site theme functional
+function setTheme(event) {
+  const root = document.documentElement;
+  const toggleBtn = event.target;
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
+  root.className = newTheme;
+
+  toggleBtn.src = `./img/${newTheme === 'dark' ? 'light-theme.svg' : 'dark-theme.svg'}`;
+}
+
+document.querySelector('.js-theme-toggle').addEventListener('click', setTheme);
